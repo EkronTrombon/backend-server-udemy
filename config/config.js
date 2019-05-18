78ffefc3a2c6a8eeac361511d9469355df147894
Xabier Remirez <ekron77@gmail.com>
2019-05-17 19:42:39 -0700

    Sección 12 - Backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+// =================================
+// Google
+// =================================
+module.exports.CLIENT_ID = '606321368262-m8ef0q5ibbhkfmipkhtci79gj12eun8b.apps.googleusercontent.com';
